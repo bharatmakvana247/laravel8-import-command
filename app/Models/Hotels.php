7766409dda31fd167b47da138dc803b3bfe1c7f6
Nene Patel <neneplusdev@gmail.com>
2020-11-05 09:04:33 +0530

    Completed import functionality

---
+++ app/Models/Hotels.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Hotels extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'image',
+        'city',
+        'address',
+        'description',
+        'stars',
+        'latitude',
+        'longitude'
+    ];
+}
